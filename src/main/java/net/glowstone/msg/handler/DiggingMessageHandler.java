8f0c968360acfe3a37d08362f2694d741ed7ebfd
zml2008 <zach@zachsthings.com>
2011-07-15 01:00:30 -0700

    Fixed BlockPlacementCodec, GlowInventory.addItem(), GlowInventory.removeItem(), and DiggingMessageHandler DiggingMessgeHandler includes BlockProperties additions as well

--- src/main/java/net/glowstone/msg/handler/DiggingMessageHandler.java
+++ src/main/java/net/glowstone/msg/handler/DiggingMessageHandler.java
@@ -3,8 +3,12 @@ package net.glowstone.msg.handler;
 import net.glowstone.block.BlockProperties;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.event.Event;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockDamageEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 
 import net.glowstone.EventFactory;
@@ -36,12 +40,50 @@ public final class DiggingMessageHandler extends MessageHandler<DiggingMessage> 
 
         Block block = world.getBlockAt(x, y, z);
 
+        BlockFace face = BlockFace.WEST;
+        switch (message.getFace()) {
+            case 0:
+                face = BlockFace.DOWN;
+                break;
+            case 1:
+                face = BlockFace.UP;
+                break;
+            case 2:
+                face = BlockFace.EAST;
+                break;
+            case 3:
+                face =  BlockFace.WEST;
+                break;
+            case 4:
+                face = BlockFace.NORTH;
+                break;
+            case 5:
+                face = BlockFace.SOUTH;
+                break;
+        }
+
+        // Notch only allows block clicks for within 6 blocks
+        if (block.getLocation().distance(player.getLocation()) > 6.0) {
+            EventFactory.onPlayerInteract(player, Action.LEFT_CLICK_AIR, block, face);
+            return;
+        }
+
+
         // Need to have some sort of verification to deal with malicious clients.
         if (message.getState() == DiggingMessage.STATE_START_DIGGING) {
+            // Runs the interact events for punching a block
+            PlayerInteractEvent interactEvent = EventFactory.onPlayerInteract(player, Action.LEFT_CLICK_BLOCK, block, face);
+            if (BlockProperties.get(block.getType()).isInteractable()) {
+                interactEvent = BlockProperties.get(block.getType()).doInteract(interactEvent);
+                if (interactEvent.useInteractedBlock() == Event.Result.ALLOW) {
+                    return;
+                }
+            }
             BlockDamageEvent event = EventFactory.onBlockDamage(player, block);
             if (!event.isCancelled()) {
                 blockBroken = event.getInstaBreak();
             }
+            blockBroken = BlockProperties.get(block.getType()).getHardness() == 0F;
         } else if (message.getState() == DiggingMessage.STATE_DONE_DIGGING) {
             BlockBreakEvent event = EventFactory.onBlockBreak(block, player);
             if (!event.isCancelled()) {
@@ -51,7 +93,8 @@ public final class DiggingMessageHandler extends MessageHandler<DiggingMessage> 
 
         if (blockBroken) {
             if (block.getType() != Material.AIR) {
-                ItemStack[] drops = BlockProperties.get(block.getType()).getDropsWithData(block.getData());
+                Byte dataDrops = block.getData();
+                ItemStack[] drops = BlockProperties.get(block.getType()).getDropsWithData((dataDrops != null) ? dataDrops : -1);
                 if (drops != null)
                 for (ItemStack stack : Arrays.asList(drops)) {
                     player.getInventory().addItem(stack);
