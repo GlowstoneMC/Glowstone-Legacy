9b9d98fcd3c152a1792de39effe596a228674e00
ZachBora <zachbora.pwegoserver@gmail.com>
2014-09-06 01:24:23 -0400

    Correct Facing Direction for chests
    The getOppositeDirection function could probably be put elsewhere to be
    reused. This one is only for 4 faces.

--- src/main/java/net/glowstone/block/blocktype/BlockChest.java
+++ src/main/java/net/glowstone/block/blocktype/BlockChest.java
@@ -25,12 +25,33 @@ public class BlockChest extends BlockContainer {
 
         MaterialData data = state.getData();
         if (data instanceof Chest) {
-            // todo: determine facing direction
-            ((Chest) data).setFacingDirection(BlockFace.EAST);
+            ((Chest) data).setFacingDirection(getOppositeDirection(player));
             state.setData(data);
         } else {
             // complain?
             GlowServer.logger.warning("Placing Chest: MaterialData was of wrong type");
+        }
+    }
+    
+    public BlockFace getOppositeDirection(GlowPlayer player) {
+        double rot = player.getLocation().getYaw() % 360;
+        
+        if (rot < 0) {
+            rot += 360.0;
+        }
+        
+        if (0 <= rot && rot < 45) {
+            return BlockFace.NORTH;
+        } else if (45 <= rot && rot < 135) {
+            return BlockFace.EAST;
+        } else if (135 <= rot && rot < 225) {
+            return BlockFace.SOUTH;
+        } else if (225 <= rot && rot < 315) {
+            return BlockFace.WEST;
+        } else if (315 <= rot && rot < 360.0) {
+            return BlockFace.NORTH;
+        } else {
+            return BlockFace.EAST;
         }
     }
 }
