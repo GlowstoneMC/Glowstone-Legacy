602e20e7745584c4122be8a26be29ff0e3e320a6
wow4201 <noemail>
2014-09-07 04:45:49 -0700

    Chest Animation (Open/Close) This feels hacky, however there are no packets to give any indication of when a specific chest is closed, just the CloseWindowMessage packet. It would be nice if mojang had a packet for this would make more sense.

--- src/main/java/net/glowstone/block/state/GlowChest.java
+++ src/main/java/net/glowstone/block/state/GlowChest.java
@@ -1,15 +1,22 @@
 package net.glowstone.block.state;
 
+import net.glowstone.GlowChunk;
 import net.glowstone.block.GlowBlock;
 import net.glowstone.block.GlowBlockState;
 import net.glowstone.block.entity.TEChest;
+import net.glowstone.entity.GlowPlayer;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.block.Chest;
 import org.bukkit.inventory.Inventory;
 
 public class GlowChest extends GlowBlockState implements Chest {
 
+	public int state;
     public GlowChest(GlowBlock block) {
         super(block);
+        state = 0;
     }
 
     private TEChest getTileEntity() {
@@ -26,4 +33,31 @@ public class GlowChest extends GlowBlockState implements Chest {
         // todo: handle double chests
         return getBlockInventory();
     }
+    
+    //closed 0, open 1
+    public int getState() {
+        return this.state;
+    }
+    
+    public void setState(Byte state) {
+        this.state = state;
+    }
+    
+    public boolean ChestAnimation(byte state) {
+        if (getBlock().getType() != Material.CHEST) {
+            return false;
+        }
+
+        Location location = getBlock().getLocation();
+
+        GlowChunk.Key key = new GlowChunk.Key(getX() >> 4, getZ() >> 4);
+        for (GlowPlayer player : getWorld().getRawPlayers()) {
+            if (player.canSee(key)) {
+                player.playChestAnimation(location, state);
+            }
+        }
+
+        return true;
+    }
+    
 }
