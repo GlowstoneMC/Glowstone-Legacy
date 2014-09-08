33a2ffc4b3da5cd7107d4329203696a8e5d78408
XXLuigiMario <joel.puig.rubio@gmail.com>
2014-09-08 19:32:14 +0200

    Convert tabs to spaces and add a little tweak.

--- src/main/java/net/glowstone/block/blocktype/BlockSnow.java
+++ src/main/java/net/glowstone/block/blocktype/BlockSnow.java
@@ -10,16 +10,16 @@ import org.bukkit.util.Vector;
 
 public class BlockSnow extends BlockType {
 
-	@Override
-	public void placeBlock(GlowPlayer player, GlowBlockState state, BlockFace face, ItemStack holding, Vector clickedLoc) {
-		GlowBlockState snowstate = state.getBlock().getRelative(BlockFace.DOWN).getState();
-		byte data = snowstate.getRawData();
-		if (snowstate.getType() == Material.SNOW && !(data >= 7)) {
-			data++;
-			snowstate.setRawData(data);
-			snowstate.update();
-		} else if (!(snowstate.getType() == Material.SNOW)) {
-			super.placeBlock(player, state, face, holding, clickedLoc);
-		}
-	}
+    @Override
+    public void placeBlock(GlowPlayer player, GlowBlockState state, BlockFace face, ItemStack holding, Vector clickedLoc) {
+        GlowBlockState snowstate = state.getBlock().getRelative(BlockFace.DOWN).getState();
+        byte data = snowstate.getRawData();
+        if (snowstate.getType() == Material.SNOW && !(data >= 7)) {
+            data++;
+            snowstate.setRawData(data);
+            snowstate.update();
+        } else if (snowstate.getType() != Material.SNOW) {
+            super.placeBlock(player, state, face, holding, clickedLoc);
+        }
+    }
 }
