68ec928a293796e94ea728a82153e9121949d174
XXLuigiMario <joel.puig.rubio@gmail.com>
2014-09-08 20:31:37 +0200

    Added little tweak.

--- src/main/java/net/glowstone/block/blocktype/BlockSnow.java
+++ src/main/java/net/glowstone/block/blocktype/BlockSnow.java
@@ -14,7 +14,7 @@ public class BlockSnow extends BlockType {
     public void placeBlock(GlowPlayer player, GlowBlockState state, BlockFace face, ItemStack holding, Vector clickedLoc) {
         GlowBlockState snowstate = state.getBlock().getRelative(BlockFace.DOWN).getState();
         byte data = snowstate.getRawData();
-        if (snowstate.getType() == Material.SNOW && !(data >= 7)) {
+        if (snowstate.getType() == Material.SNOW && data < 7) {
             data++;
             snowstate.setRawData(data);
             snowstate.update();
