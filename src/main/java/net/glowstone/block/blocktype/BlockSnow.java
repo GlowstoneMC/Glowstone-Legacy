9ea9938cb58c0cf9b3f1e5385ba56e15e6f6310b
XXLuigiMario <joel.puig.rubio@gmail.com>
2014-09-08 19:11:02 +0200

    Create SnowBlock.java to fix snow layer placement.

---
+++ src/main/java/net/glowstone/block/blocktype/BlockSnow.java
@@ -0,0 +1,25 @@
+package net.glowstone.block.blocktype;
+
+import net.glowstone.block.GlowBlockState;
+import net.glowstone.entity.GlowPlayer;
+
+import org.bukkit.Material;
+import org.bukkit.block.BlockFace;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.util.Vector;
+
+public class BlockSnow extends BlockType {
+
+	@Override
+	public void placeBlock(GlowPlayer player, GlowBlockState state, BlockFace face, ItemStack holding, Vector clickedLoc) {
+		GlowBlockState snowstate = state.getBlock().getRelative(BlockFace.DOWN).getState();
+		byte data = snowstate.getRawData();
+		if (snowstate.getType() == Material.SNOW && !(data >= 7)) {
+			data++;
+			snowstate.setRawData(data);
+			snowstate.update();
+		} else if (!(snowstate.getType() == Material.SNOW)) {
+			super.placeBlock(player, state, face, holding, clickedLoc);
+		}
+	}
+}
