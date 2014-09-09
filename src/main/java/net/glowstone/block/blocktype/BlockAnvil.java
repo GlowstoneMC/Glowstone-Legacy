869ad450f99a6e2f6dbf030cd1a5173238c85a2e
wow4201 <noemail>
2014-09-08 21:29:39 -0700

    Play anvil sound when placed

--- src/main/java/net/glowstone/block/blocktype/BlockAnvil.java
+++ src/main/java/net/glowstone/block/blocktype/BlockAnvil.java
@@ -1,10 +1,17 @@
 package net.glowstone.block.blocktype;
 
+import net.glowstone.GlowServer;
 import net.glowstone.block.GlowBlock;
+import net.glowstone.block.GlowBlockState;
 import net.glowstone.entity.GlowPlayer;
+
 import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.block.BlockFace;
 import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.material.Dispenser;
+import org.bukkit.material.MaterialData;
 import org.bukkit.util.Vector;
 
 public class BlockAnvil extends BlockType {
@@ -12,4 +19,11 @@ public class BlockAnvil extends BlockType {
     public boolean blockInteract(GlowPlayer player, GlowBlock block, BlockFace face, Vector clickedLoc) {
         return player.openBlockWindow(block.getLocation(), false, Material.ANVIL, InventoryType.ANVIL) != null;
     }
+    
+    @Override
+    public void placeBlock(GlowPlayer player, GlowBlockState state, BlockFace face, ItemStack holding, Vector clickedLoc) {
+        super.placeBlock(player, state, face, holding, clickedLoc);
+        state.getBlock().getWorld().playSound(state.getBlock().getLocation(), Sound.ANVIL_LAND, 1, 0.8F);
+    }
+    
 }
