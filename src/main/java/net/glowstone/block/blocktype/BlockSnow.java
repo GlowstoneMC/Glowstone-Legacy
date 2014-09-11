0b8c0f4983bf6740dae850888a1ff8108e9d7f93
XXLuigiMario <joel.puig.rubio@gmail.com>
2014-09-11 22:54:33 +0200

    Updated the code to comply with the project's code style.

--- src/main/java/net/glowstone/block/blocktype/BlockSnow.java
+++ src/main/java/net/glowstone/block/blocktype/BlockSnow.java
@@ -12,13 +12,21 @@ public class BlockSnow extends BlockType {
 
     @Override
     public void placeBlock(GlowPlayer player, GlowBlockState state, BlockFace face, ItemStack holding, Vector clickedLoc) {
+
+        /* We get the block state and data value of the block under the target. */
         GlowBlockState snowstate = state.getBlock().getRelative(BlockFace.DOWN).getState();
         byte data = snowstate.getRawData();
-        if (snowstate.getType() == Material.SNOW && data < 7) {
+
+        if ((snowstate.getType() == Material.SNOW) && (data < 7)) {
+
+            /* If the block we got is snow and it does not have more than 7 layers, increment the block's data value. */
             data++;
+
             snowstate.setRawData(data);
             snowstate.update();
         } else if (snowstate.getType() != Material.SNOW) {
+
+            /* Else if the block is not snow, place the block on top of the snow. */
             super.placeBlock(player, state, face, holding, clickedLoc);
         }
     }
