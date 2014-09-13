790bf05275a23473ec3f15ed9a8a2d6e4bcceb1a
Tonodus <tonodus@freenet.de>
2014-09-13 11:57:02 +0200

    added directional placing for BlockFenceGates

--- src/main/java/net/glowstone/block/blocktype/BlockFenceGate.java
+++ src/main/java/net/glowstone/block/blocktype/BlockFenceGate.java
@@ -2,14 +2,38 @@ package net.glowstone.block.blocktype;
 
 import net.glowstone.GlowServer;
 import net.glowstone.block.GlowBlock;
+import net.glowstone.block.GlowBlockState;
 import net.glowstone.entity.GlowPlayer;
 import org.bukkit.block.BlockFace;
 import org.bukkit.block.BlockState;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.material.Directional;
 import org.bukkit.material.Gate;
 import org.bukkit.material.MaterialData;
 import org.bukkit.util.Vector;
 
 public class BlockFenceGate extends BlockOpenable {
+    private static final BlockFace[] faces = {BlockFace.EAST, BlockFace.SOUTH, BlockFace.WEST, BlockFace.NORTH};
+
+    @Override
+    public void placeBlock(GlowPlayer player, GlowBlockState state, BlockFace face, ItemStack holding, Vector clickedLoc) {
+        super.placeBlock(player, state, face, holding, clickedLoc);
+
+        MaterialData materialData = state.getData();
+        if (materialData instanceof Directional) {
+            Directional directional = (Directional) materialData;
+            float yaw = player.getLocation().getYaw();
+            directional.setFacingDirection(blockFaceFromYaw(yaw));
+            state.update(true);
+        }
+    }
+
+    private static BlockFace blockFaceFromYaw(float yaw) {
+        yaw = yaw % 360;
+        if (yaw < 0)
+            yaw += 360;
+        return faces[Math.round(yaw / 90f) & 0x3];
+    }
 
     @Override
     public boolean blockInteract(GlowPlayer player, GlowBlock block, BlockFace face, Vector clickedLoc) {
