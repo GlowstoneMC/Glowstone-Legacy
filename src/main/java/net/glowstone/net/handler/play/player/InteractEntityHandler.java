0dabb7a6dec3db7596acef2d0bf4e7b88d991a0d
wow4201 <noemail>
2014-09-13 17:11:35 -0700

    Item Frame Full Implementation

--- src/main/java/net/glowstone/net/handler/play/player/InteractEntityHandler.java
+++ src/main/java/net/glowstone/net/handler/play/player/InteractEntityHandler.java
@@ -1,12 +1,25 @@
 package net.glowstone.net.handler.play.player;
 
 import com.flowpowered.networking.MessageHandler;
+
+import net.glowstone.GlowServer;
+import net.glowstone.GlowWorld;
+import net.glowstone.entity.GlowEntity;
+import net.glowstone.entity.GlowPlayer;
 import net.glowstone.net.GlowSession;
 import net.glowstone.net.message.play.player.InteractEntityMessage;
 
 public final class InteractEntityHandler implements MessageHandler<GlowSession, InteractEntityMessage> {
     @Override
     public void handle(GlowSession session, InteractEntityMessage message) {
-        // todo
+       
+    	final GlowPlayer player = session.getPlayer();
+        if (player == null)
+            return;
+        
+    	final GlowWorld world = player.getWorld();
+        final GlowEntity Entity = world.getEntityManager().getEntity(message.getId());
+        
+        Entity.entityInteract(player, message);
     }
 }
