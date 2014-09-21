5239cdc16878b86acfca8e2566533dbd7a81cf08
Aaron Hill <aa1ronham@gmail.com>
2014-09-21 05:24:49 -0400

    Use only one class

--- src/main/java/net/glowstone/block/blocktype/BlockDaylightDetector.java
+++ src/main/java/net/glowstone/block/blocktype/BlockDaylightDetector.java
@@ -7,10 +7,14 @@ import org.bukkit.block.BlockFace;
 import org.bukkit.util.Vector;
 
 public class BlockDaylightDetector extends BlockType {
-    
+
     @Override    
     public boolean blockInteract(GlowPlayer player, GlowBlock block, BlockFace face, Vector clickedLoc) {
-        block.setType(Material.DAYLIGHT_DETECTOR_INVERTED);
+        if (block.getType() == Material.DAYLIGHT_DETECTOR) {
+            block.setType(Material.DAYLIGHT_DETECTOR_INVERTED);
+        } else {
+            block.setType(Material.DAYLIGHT_DETECTOR);
+        }
         return true;
     }
 }
