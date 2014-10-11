6a0c1a2eca08e8abfbc3a09ab856e15a3babff07
Gareth Coles <gdude2002@gmail.com>
2014-10-11 22:19:26 +0100

    Add util for fetching UUIDs for usernames from Mojang

--- src/main/java/net/glowstone/util/UuidUtils.java
+++ src/main/java/net/glowstone/util/UuidUtils.java
@@ -1,11 +1,21 @@
 package net.glowstone.util;
 
+import net.glowstone.GlowServer;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.UUID;
 
 /**
  * Utility methods for dealing with UUIDs.
  */
 public final class UuidUtils {
+
+    private static String getURL = "https://api.mojang.com/users/profiles/minecraft/";
 
     private UuidUtils() {}
 
@@ -19,4 +29,18 @@ public final class UuidUtils {
         return uuid.toString().replace("-", "");
     }
 
+    public static UUID fetchUUID(String username) {
+        try {
+            URLConnection connection = new URL(getURL + username).openConnection();
+            JSONObject json;
+
+            try (InputStream is = connection.getInputStream()) {
+                json = (JSONObject) new JSONParser().parse(new InputStreamReader(is));
+                return fromFlatString((String) json.get("id"));
+            }
+        } catch (Exception e) {
+            GlowServer.logger.warning("Failed to fetch UUID for \"" + username + "\"");
+            return null;
+        }
+    }
 }
