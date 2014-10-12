7b1ebaca3c6c767a7880b2198d8eb8a4b4ce7b06
mastercoms <circuitsoft@openmailbox.org>
2014-10-12 14:43:34 -0400

    Added imports and removed dupe keys

--- src/main/java/net/glowstone/util/ServerConfig.java
+++ src/main/java/net/glowstone/util/ServerConfig.java
@@ -10,7 +10,9 @@ import org.yaml.snakeyaml.error.YAMLException;
 
 import java.io.*;
 import java.net.URL;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.logging.Level;
@@ -373,9 +375,6 @@ public final class ServerConfig {
         RESOURCE_PACK("game.resource-pack", "", Migrate.PROPS, "resource-pack"),
         RESOURCE_PACK_HASH("game.resource-pack-hash", "", Migrate.PROPS, "resource-pack-hash"),
         SNOOPER_ENABLED("server.snooper-enabled", false, Migrate.PROPS, "snooper-enabled"),
-
-        LIBRARY("libraries.depend", Arrays.asList("org.xerial:sqlite-jdbc:3.7.2", "mysql:mysql-connector-java:5.1.14", "org.apache.logging.log4j:log4j-core:2.0", "org.apache.logging.log4j:log4j-api:2.0")),
-        LIBRARY_REPO("libraries.repo", "http://repo1.maven.org/maven2/"),
 
         LIBRARY("libraries.depend", Arrays.asList("org.xerial:sqlite-jdbc:3.7.2", "mysql:mysql-connector-java:5.1.14", "org.apache.logging.log4j:log4j-core:2.0", "org.apache.logging.log4j:log4j-api:2.0")),
         LIBRARY_REPO("libraries.repo", "http://repo1.maven.org/maven2/"),
