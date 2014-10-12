5f24d584214d2c4739f8a9f939ea8a4a453951ab
mastercoms <circuitsoft@openmailbox.org>
2014-10-12 13:54:53 -0400

    add dolibs method
    doLibs starts the library manager. The library manager starts on startup, after everything else.

--- src/main/java/net/glowstone/GlowServer.java
+++ src/main/java/net/glowstone/GlowServer.java
@@ -423,6 +423,8 @@ public final class GlowServer implements Server {
         enablePlugins(PluginLoadOrder.POSTWORLD);
         commandMap.registerServerAliases();
         scheduler.start();
+        doLibs();
+
     }
 
     /**
@@ -1225,6 +1227,13 @@ public final class GlowServer implements Server {
     @Override
     public GlowMapView createMap(World world) {
         throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    ////////////////////////////////////////////////////////////////////////////
+    // Library Manager
+
+    public LibraryManager doLibs() {
+        return new LibraryManager(this);
     }
 
     ////////////////////////////////////////////////////////////////////////////
