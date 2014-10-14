cfee79f096097b00b8e801dfc0c0d3a26de17eb7
jimmikaelkael <jimmikaelkael@wanadoo.fr>
2014-10-14 17:40:41 +0200

    Add BlockStem, remove BlockMelonStem and BlockPumpkinStem

---
+++ src/main/java/net/glowstone/block/blocktype/BlockStem.java
@@ -0,0 +1,46 @@
+package net.glowstone.block.blocktype;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Random;
+
+import org.bukkit.CropState;
+import org.bukkit.Material;
+import org.bukkit.block.BlockFace;
+import org.bukkit.inventory.ItemStack;
+
+import net.glowstone.block.GlowBlock;
+
+public class BlockStem extends BlockPlant {
+    private Material fruitType;
+    private Material seedsType;
+    private final Random random = new Random();
+
+    public BlockStem(Material plantType) {
+        if (plantType.equals(Material.MELON_STEM)) {
+            fruitType = Material.MELON_BLOCK;
+            seedsType = Material.MELON_SEEDS;
+        } else if (plantType.equals(Material.PUMPKIN_STEM)) {
+            fruitType = Material.PUMPKIN;
+            seedsType = Material.PUMPKIN_SEEDS;
+        }
+    }
+
+    @Override
+    public boolean canPlaceAt(GlowBlock block, BlockFace against) {
+        if (block.getRelative(BlockFace.DOWN).getType().equals(Material.SOIL)) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public Collection<ItemStack> getDrops(GlowBlock block) {
+        if (block.getState().getRawData() >= CropState.RIPE.ordinal()) {
+            return Collections.unmodifiableList(Arrays.asList(new ItemStack(seedsType, random.nextInt(4))));
+        } else {
+            return Collections.unmodifiableList(Arrays.asList(new ItemStack[0]));
+        }
+    }
+}
