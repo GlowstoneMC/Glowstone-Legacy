44ce6e715bdc02a7a75537d420071f663fa0b62b
kaenganxt <kaenganxt@yahoo.de>
2014-10-15 17:35:59 +0200

    Add redstone functions for levers

--- src/main/java/net/glowstone/block/blocktype/BlockLever.java
+++ src/main/java/net/glowstone/block/blocktype/BlockLever.java
@@ -1,5 +1,6 @@
 package net.glowstone.block.blocktype;
 
+import net.glowstone.RSManager;
 import net.glowstone.block.GlowBlock;
 import net.glowstone.block.GlowBlockState;
 import net.glowstone.entity.GlowPlayer;
@@ -14,6 +15,99 @@ public class BlockLever extends BlockAttachable {
 
     public BlockLever() {
         setDrops(new ItemStack(Material.LEVER));
+    }
+
+    @Override
+    public boolean canBlockEmitPower(GlowBlock block, BlockFace face, boolean isDirect) {
+        final MaterialData data = block.getState().getData();
+        if (data instanceof Lever) {
+            final Lever l = (Lever) data;
+            if (!l.isPowered()) {
+                return false;
+            }
+            //The lever powers the attached block direct
+            if (l.getAttachedFace().equals(face)) {
+                return true;
+            } else if (isDirect) { //All other faces aren't powered direct
+                return false;
+            }
+            return true; //Emits power to all directions, but not direct
+        }
+        return false;
+    }
+
+    @Override
+    public boolean isRedSource(GlowBlock block) {
+        return true;
+    }
+
+    @Override
+    public void traceBlockPowerInit(GlowBlock block, RSManager rsManager) {
+        final MaterialData data = block.getState().getData();
+        if (data instanceof Lever) {
+            final Lever l = (Lever) data;
+            if (l.isPowered()) {
+                rsManager.setBlockPower(block, 15);
+            } else {
+                rsManager.setBlockPower(block, 0);
+            }
+        }
+    }
+
+    @Override
+    public void traceBlockPowerStart(GlowBlock block, RSManager rsManager) {
+        final MaterialData data = block.getState().getData();
+        if (data instanceof Lever) {
+            final Lever l = (Lever) data;
+            if (!l.isPowered()) return;
+            // Trace in all directions.
+            traceBlockPowerFromLever(block, rsManager, l.getAttachedFace(), BlockFace.UP);
+            traceBlockPowerFromLever(block, rsManager, l.getAttachedFace(), BlockFace.DOWN);
+            traceBlockPowerFromLever(block, rsManager, l.getAttachedFace(), BlockFace.NORTH);
+            traceBlockPowerFromLever(block, rsManager, l.getAttachedFace(), BlockFace.SOUTH);
+            traceBlockPowerFromLever(block, rsManager, l.getAttachedFace(), BlockFace.WEST);
+            traceBlockPowerFromLever(block, rsManager, l.getAttachedFace(), BlockFace.EAST);
+        }
+    }
+    
+    private void traceBlockPowerFromLever(GlowBlock srcBlock, RSManager rsManager, BlockFace directDir, BlockFace toDir) {
+        if (toDir.equals(directDir)) {
+            rsManager.traceFromBlock(srcBlock, toDir, 15, true);
+        } else {
+            rsManager.traceFromBlock(srcBlock, toDir, 15, false);
+        }
+    }
+
+    @Override
+    public void traceBlockPowerEnd(GlowBlock block, RSManager rsManager, int power) {
+        if (power == 15) {
+            rsManager.addSource(block);
+        }
+    }
+
+    public void setAttachedFace(final Lever lever, final BlockFace attachedFace) {
+        byte data = lever.getData();
+        switch (attachedFace) {
+            case WEST:
+                data |= 1;
+                break;
+            case EAST:
+                data |= 2;
+                break;
+            case NORTH:
+                data |= 3;
+                break;
+            case SOUTH:
+                data |= 4;
+                break;
+            case DOWN:
+                data |= 5; // or 6
+                break;
+            case UP:
+                data |= 7; // or 0
+                break;
+        }
+        lever.setData(data);
     }
 
     @Override
