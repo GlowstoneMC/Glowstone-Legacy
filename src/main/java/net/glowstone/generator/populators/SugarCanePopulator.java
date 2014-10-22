152f0171c9711bee8cbea3effc1af769c9d89eb4
jimmikaelkael <jimmikaelkael@wanadoo.fr>
2014-10-22 12:46:45 +0200

    Fix equality checks

--- src/main/java/net/glowstone/generator/populators/SugarCanePopulator.java
+++ src/main/java/net/glowstone/generator/populators/SugarCanePopulator.java
@@ -21,23 +21,23 @@ public class SugarCanePopulator extends BlockPopulator {
             for (int j = 0; j < 20; j++) {
                 int x = sourceX + random.nextInt(4) - random.nextInt(4);
                 int z = sourceZ + random.nextInt(4) - random.nextInt(4);
-                if (world.getBlockAt(x, sourceY, z).getType().equals(Material.AIR)
+                if (world.getBlockAt(x, sourceY, z).getType() == Material.AIR
                         // needs a directly adjacent stationary water block
-                        && (world.getBlockAt(x - 1, sourceY - 1, z).getType().equals(Material.STATIONARY_WATER)
-                        || world.getBlockAt(x + 1, sourceY - 1, z).getType().equals(Material.STATIONARY_WATER)
-                        || world.getBlockAt(x, sourceY - 1, z - 1).getType().equals(Material.STATIONARY_WATER)
-                        || world.getBlockAt(x, sourceY - 1, z + 1).getType().equals(Material.STATIONARY_WATER)
+                        && (world.getBlockAt(x - 1, sourceY - 1, z).getType() == Material.STATIONARY_WATER
+                        || world.getBlockAt(x + 1, sourceY - 1, z).getType() == Material.STATIONARY_WATER
+                        || world.getBlockAt(x, sourceY - 1, z - 1).getType() == Material.STATIONARY_WATER
+                        || world.getBlockAt(x, sourceY - 1, z + 1).getType() == Material.STATIONARY_WATER
                         // or a directly adjacent flowing water block
-                        || world.getBlockAt(x - 1, sourceY - 1, z).getType().equals(Material.WATER)
-                        || world.getBlockAt(x + 1, sourceY - 1, z).getType().equals(Material.WATER)
-                        || world.getBlockAt(x, sourceY - 1, z - 1).getType().equals(Material.WATER)
-                        || world.getBlockAt(x, sourceY - 1, z + 1).getType().equals(Material.WATER))) {
+                        || world.getBlockAt(x - 1, sourceY - 1, z).getType() == Material.WATER
+                        || world.getBlockAt(x + 1, sourceY - 1, z).getType() == Material.WATER
+                        || world.getBlockAt(x, sourceY - 1, z - 1).getType() == Material.WATER
+                        || world.getBlockAt(x, sourceY - 1, z + 1).getType() == Material.WATER)) {
 
                     for (int n = 0; n <= random.nextInt(random.nextInt(3) + 1) + 1; n++) {
                         final Material type = world.getBlockAt(x, sourceY - 1, z).getType();
-                        if (type.equals(Material.SUGAR_CANE_BLOCK) || type.equals(Material.GRASS)
-                                || (type.equals(Material.DIRT) && world.getBlockAt(x, sourceY - 1, z).getData() == 0)
-                                || type.equals(Material.SAND)) {
+                        if (type == Material.SUGAR_CANE_BLOCK || type == Material.GRASS
+                                || (type == Material.DIRT && world.getBlockAt(x, sourceY - 1, z).getData() == 0)
+                                || type == Material.SAND) {
                             final Block block = world.getBlockAt(x, sourceY + n, z);
                             block.setType(Material.SUGAR_CANE_BLOCK);
                             block.setData((byte) 0);
