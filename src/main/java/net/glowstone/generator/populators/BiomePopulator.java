8fdcf4527faa279b394ed5638486f638f01c0588
jimmikaelkael <jimmikaelkael@wanadoo.fr>
2014-10-22 21:27:49 +0200

    Add DeadBushDecorator

--- src/main/java/net/glowstone/generator/populators/BiomePopulator.java
+++ src/main/java/net/glowstone/generator/populators/BiomePopulator.java
@@ -1,5 +1,6 @@
 package net.glowstone.generator.populators;
 
+import net.glowstone.generator.decorators.DeadBushDecorator;
 import net.glowstone.generator.decorators.Decorator;
 import net.glowstone.generator.decorators.SugarCaneDecorator;
 
@@ -8,22 +9,30 @@ import org.bukkit.World;
 import org.bukkit.block.Biome;
 import org.bukkit.generator.BlockPopulator;
 
-import java.util.Collection;
-import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class BiomePopulator extends BlockPopulator {
 
-    private Collection<Decorator> decorators = new HashSet<Decorator>();
+    private List<Decorator> decorators = new ArrayList<Decorator>();
 
     public BiomePopulator() {
-        addDecorator(
-                new SugarCaneDecorator()
-                        .setDefaultAmount(10)
-                        .setBiomeAmount(Biome.DESERT, 60)
-                        .setBiomeAmount(Biome.MESA, 13)
-                        .setBiomeAmount(Biome.SWAMPLAND, 20)
-        );
+
+        // the order is important
+
+        addDecorator(new DeadBushDecorator()
+                .setDefaultAmount(10)
+                .setBiomeAmount(Biome.DESERT, 2)
+                .setBiomeAmount(Biome.MESA, 20)
+                .setBiomeAmount(Biome.SWAMPLAND, 1)
+                .setBiomeAmount(Biome.TAIGA, 1));
+
+        addDecorator(new SugarCaneDecorator()
+                .setDefaultAmount(10)
+                .setBiomeAmount(Biome.DESERT, 60)
+                .setBiomeAmount(Biome.MESA, 13)
+                .setBiomeAmount(Biome.SWAMPLAND, 20));
     }
 
     @Override
