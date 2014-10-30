a4f60568a9c8fb87936153d28ab65bc82930e540
Aaron Hill <aa1ronham@gmail.com>
2014-10-30 18:45:16 -0400

    Fire PlayerItemHeldEvent when player switches held item

--- src/main/java/net/glowstone/net/handler/play/inv/HeldItemHandler.java
+++ src/main/java/net/glowstone/net/handler/play/inv/HeldItemHandler.java
@@ -1,8 +1,11 @@
 package net.glowstone.net.handler.play.inv;
 
 import com.flowpowered.networking.MessageHandler;
+import net.glowstone.EventFactory;
+import net.glowstone.entity.GlowPlayer;
 import net.glowstone.net.GlowSession;
 import net.glowstone.net.message.play.inv.HeldItemMessage;
+import org.bukkit.event.player.PlayerItemHeldEvent;
 
 public final class HeldItemHandler implements MessageHandler<GlowSession, HeldItemMessage> {
     @Override
@@ -10,6 +13,16 @@ public final class HeldItemHandler implements MessageHandler<GlowSession, HeldIt
         final int slot = message.getSlot();
         if (slot < 0 || slot > 8) // sanity check
             return;
-        session.getPlayer().getInventory().setRawHeldItemSlot(slot);
+        GlowPlayer player = session.getPlayer();
+
+        PlayerItemHeldEvent event = new PlayerItemHeldEvent(player, player.getInventory().getHeldItemSlot(), slot);
+        EventFactory.callEvent(event);
+
+        if (!event.isCancelled()) {
+            session.getPlayer().getInventory().setRawHeldItemSlot(slot);
+        } else {
+            // This sends a packet to the player telling them to switch their held slot
+            session.getPlayer().getInventory().setHeldItemSlot(player.getInventory().getHeldItemSlot());
+        }
     }
 }
