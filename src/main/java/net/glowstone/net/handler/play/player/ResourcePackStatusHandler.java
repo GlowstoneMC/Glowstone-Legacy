f3281d9891bb8353e43b38bf628f8a15e2bd873c
Aaron Hill <aa1ronham@gmail.com>
2014-11-03 16:30:16 -0500

    Don't use magic values in Glowkit

--- src/main/java/net/glowstone/net/handler/play/player/ResourcePackStatusHandler.java
+++ src/main/java/net/glowstone/net/handler/play/player/ResourcePackStatusHandler.java
@@ -3,6 +3,7 @@ package net.glowstone.net.handler.play.player;
 import com.flowpowered.networking.MessageHandler;
 import net.glowstone.EventFactory;
 import net.glowstone.GlowServer;
+import net.glowstone.constants.GlowResourcePackStatus;
 import net.glowstone.net.GlowSession;
 import net.glowstone.net.message.play.player.ResourcePackStatusMessage;
 import org.bukkit.event.player.PlayerResourcePackStatusEvent;
@@ -10,6 +11,6 @@ import org.bukkit.event.player.PlayerResourcePackStatusEvent;
 public final class ResourcePackStatusHandler implements MessageHandler<GlowSession, ResourcePackStatusMessage> {
     @Override
     public void handle(GlowSession session, ResourcePackStatusMessage message) {
-        EventFactory.callEvent(new PlayerResourcePackStatusEvent(session.getPlayer(), PlayerResourcePackStatusEvent.Status.valueOf(message.getResult()), message.getHash()));
+        EventFactory.callEvent(new PlayerResourcePackStatusEvent(session.getPlayer(), GlowResourcePackStatus.getStatus(message.getResult()), message.getHash()));
     }
 }
